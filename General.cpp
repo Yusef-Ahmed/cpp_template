a66f8da027b40c925da40d95dfa28fbf56952d13
Youssef A. Saad <125928861+Yusef-Ahmed@users.noreply.github.com>
2024-03-01 14:26:10 +0200

    Create General.cpp

---
+++ General.cpp
@@ -0,0 +1,58 @@
+/*
+ ████████╗░█████╗░███╗░░██╗░░░░░██╗██╗██████╗░░█████╗░██╗░░░██╗
+ ╚══██╔══╝██╔══██╗████╗░██║░░░░░██║██║██╔══██╗██╔══██╗██║░░░██║
+ ░░░██║░░░███████║██╔██╗██║░░░░░██║██║██████╔╝██║░░██║██║░░░██║
+ ░░░██║░░░██╔══██║██║╚████║██╗░░██║██║██╔══██╗██║░░██║██║░░░██║
+ ░░░██║░░░██║░░██║██║░╚███║╚█████╔╝██║██║░░██║╚█████╔╝╚██████╔╝
+ ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝░╚════╝░╚═╝╚═╝░░╚═╝░╚════╝░░╚═════╝░
+*/
+#include <bits/stdc++.h>
+#define endl "\n"
+#define F first
+#define S second
+#define pb push_back
+#define all(v)  v.begin(), v.end()
+#define allr(v) v.rbegin(),v.rend()
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int,int>
+#define pdd pair<double,double>
+#define pll pair<ll,ll>
+using namespace std;
+void TANJIR0U();
+const int mod=1e9+7, oo=0x3f3f3f3f, N=1e5+9, 
+LOG=18, P1=31, P2=37;
+const ll loo=0x3f3f3f3f3f3f3f3f;
+//string steps = "RLUD";
+//int dx[4] = {0, 0, -1, 1};
+//int dy[4] = {1, -1, 0, 0};
+//int dx[8] = {1, 0, -1, 1, -1, 1, 0, -1};
+//int dy[8] = {1, 1, 1, 0, 0, -1, -1, -1};
+
+void solve() {
+    
+}
+
+int main() {
+    TANJIR0U();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
+
+void TANJIR0U() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    //    freopen("input.in", "r", stdin);
+    //    freopen("output.out", "w", stdout);
+#endif
+}
